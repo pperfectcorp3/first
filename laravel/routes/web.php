0114d83d8957eed92217ec793c7245d83d09c492
gedeontimothy <gedeonbateko3@gmail.com>
2024-01-17 01:16:02 +0100

    [UPDATE] - Mise à jour de la route `/dashboard` ayant le nom de `dashboard` en chageant le path de sa view et en y ajoutant le middleware `user-access:admin`

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -19,8 +19,8 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    return view('pages.admin.dashboard');
+})->middleware(['auth', 'verified', 'user-access:admin'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
