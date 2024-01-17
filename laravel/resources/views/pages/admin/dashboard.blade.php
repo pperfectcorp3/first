51647497162b0478fdca1bdec7a3cabc4a70baec
gedeontimothy <gedeonbateko3@gmail.com>
2024-01-17 01:23:36 +0100

    [RENAME] Views | Move - Déplacement de plusieurs vues dont de celui du dashboard et de tout ce qui constistue les vues du profile

--- laravel/resources/views/pages/admin/dashboard.blade.php
+++ laravel/resources/views/pages/admin/dashboard.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-app-layout :title="'Dashboard - ' . ucfirst(auth()->user()->type)">
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    {{ __("You're logged in! " . auth()->user()->id) }}
                 </div>
             </div>
         </div>
